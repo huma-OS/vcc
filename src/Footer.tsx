99143f4c336c384d08a367837cb5cf78c7267099
huma-OS <cheifomera@yahoo.com>
2024-07-10 17:53:22 +0100

    VCC0002: initial mob first navbar

---
+++ src/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return ( 
+    <div className="footer">
+      <p>this will have icon and copy developed by</p>
+    </div>
+   );
+}
+ 
+export default Footer;
---
+++ src/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return ( 
+    <div className="home">
+      <p>this will have six cards</p>
+    </div>
+   );
+}
+ 
+export default Home;
