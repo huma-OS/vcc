909e2b35ed338badab8108bfd59be8961d665223
huma-OS <cheifomera@yahoo.com>
2024-07-25 20:59:14 +0100

    VCC00006: render gallery

--- src/Gallery.tsx
+++ src/Gallery.tsx
@@ -1,10 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import GalleryList from "./GalleryList";
+interface GallerySlide {
+  title: string;
+  imgUrl: string;
+  id: number;
+}
 
 const Gallery = () => {
+  const [gallerySlides, setGallerySlides ] = useState<GallerySlide[]>([]);
+  const [isPending, setIsPending] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch('http://localhost:8000/videography')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setGallerySlides(data);
+        setIsPending(false);
+      })
+    }, 5000)
+  }, []);
   return (  
-    <div className="gallery">
-      <p>This will be the template for all gallery cards for all pages blog list like</p>
-    </div>
+    <>
+    {/* <h2 className="titles-title">PHOTOGRAPHY</h2> */}
+    <ul className="slide-models slide-models-photo gallery">
+               
+   {isPending ? (
+    <>
+    {[...Array(5)].map((_, index) => (
+        <li className="slide-model-links" key={index}>
+        <div className="slide-model slide-shadow slide-model-photo">
+          <figure className="slide-model-elements card loading">
+            <img className="slide-model-element-img slide-model-element-img-photo image" alt="" />
+          </figure>
+        </div>
+      </li>
+      ))}
+  </>
+) : (
+  <GalleryList gallerySlides={gallerySlides} />
+)}
+      {/* {gallerySlides && <GalleryList gallerySlides={gallerySlides} />} */}
+      {/* <GalleryList gallerySlides={gallerySlides.filter((gallerySlide) => gallerySlide.title === 'PHOTOGRAPHY' )} /> */}
+    </ul>
+    </>
   );
 }
  
