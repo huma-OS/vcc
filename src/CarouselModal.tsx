f0d33c087bd2a009c102d18f2f0e48a7bf0afe57
huma-OS <cheifomera@yahoo.com>
2024-08-02 22:55:57 +0100

    VCC00013: tablet large and small desktop styles

--- src/CarouselModal.tsx
+++ src/CarouselModal.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Carousel } from 'react-responsive-carousel'
-import "react-responsive-carousel/lib/styles/carousel.min.css"
+import { Carousel } from 'react-responsive-carousel';
+import { useState } from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 interface CarouselModalProps {
   images: string[];
@@ -8,19 +9,34 @@ interface CarouselModalProps {
   onClose: () => void;
 }
 
-const CarouselModal: React.FC<CarouselModalProps> = ({images, selectedIndex, onClose}) => {
-  return ( 
-    <div className="carousel-modal slide-model-links">
-    <button className="close-button" onClick={onClose}></button>
-    <Carousel selectedItem={selectedIndex} showThumbs={false} useKeyboardArrows>
-      {images.map((imgUrl, index) => (
-        <div key={index}>
-          <img src={imgUrl} alt={`Slide ${index + 1}`} />
-        </div>
-      ))}
-    </Carousel>
-  </div>
-   );
-}
- 
+const CarouselModal = ({ images, selectedIndex, onClose }: CarouselModalProps) => {
+  const [currentIndex, setCurrentIndex] = useState(selectedIndex);
+
+  const handleOnChange = (index: number) => {
+    setCurrentIndex(index);
+  };
+
+  return (
+    <div className="carousel-modal">
+      <button className="close-button" onClick={onClose}></button>
+      <div className="carousel-status">
+        {`${currentIndex + 1}/${images.length}`}
+      </div>
+      <Carousel
+        selectedItem={selectedIndex}
+        showThumbs={false}
+        useKeyboardArrows
+        showStatus={false}
+        onChange={handleOnChange}
+      >
+        {images.map((imgUrl, index) => (
+          <div key={index}>
+            <img src={imgUrl} alt={`Slide ${index + 1}`} />
+          </div>
+        ))}
+      </Carousel>
+    </div>
+  );
+};
+
 export default CarouselModal;
