e34878e0fb6017dc56dd5ff7c1d004928574a47c
huma-OS <cheifomera@yahoo.com>
2024-08-13 21:57:50 +0100

    VCC00017: webdev pages sorted

---
+++ src/WebDevAfrica.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const WebDevAfrica = () => {
+  return ( 
+    <div>
+      <h1>AFRICA</h1>
+      <p>This is a more elaborate web development app for page 1.</p>
+      {/* Add more components and functionality as needed */}
+    </div>
+   );
+}
+ 
+export default WebDevAfrica;
---
+++ src/WebDevInvoice.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const WebDevInvoice = () => {
+  return ( 
+    <div>
+    <h1>INVOICE</h1>
+    <p>This is a more elaborate web development app for page 1.</p>
+    {/* Add more components and functionality as needed */}
+  </div>
+   );
+}
+ 
+export default WebDevInvoice;
