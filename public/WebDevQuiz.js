edd2527a18853d18f5dcdaa719f3faac4bafaf91
huma-OS <cheifomera@yahoo.com>
2024-09-01 19:44:54 +0100

    VCC00019: web dev1 quiz and africa pages as well as image change

---
+++ public/WebDevQuiz.js
@@ -0,0 +1,42 @@
+(function() {
+  const form = document.querySelector('.quiz-form');
+  const result = document.querySelector('.result');
+
+  if (form && result) {
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      let score = 0;
+      const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+
+      // Assuming correctAnswers is available globally or passed in via React
+      const correctAnswers = window.correctAnswers || ['A', 'D', 'C', 'B']; // Default values as a fallback
+
+      // Check answers
+      userAnswers.forEach((answer, index) => {
+        if (answer === correctAnswers[index]) {
+          score += 25;
+        }
+      });
+
+      // Show results on page
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+      result.classList.remove('d-none');
+
+      let output = 0;
+      const timer = setInterval(() => {
+        const scoreDisplay = result.querySelector('span');
+        if (scoreDisplay) {
+          scoreDisplay.textContent = `${output}%`;
+        }
+        if (output === score) {
+          clearInterval(timer);
+        } else {
+          output++;
+        }
+      }, 10);
+    });
+  }
+
+  console.log('hello');
+})();
--- src/WebDevAfrica.tsx
+++ src/WebDevAfrica.tsx
@@ -1,12 +1,93 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const WebDevAfrica = () => {
+const WebDevAfrica: React.FC = () => {
+  const [cities, setCities] = useState<any[]>([]);
+  const [query, setQuery] = useState<string>('');
+  const [filteredCities, setFilteredCities] = useState<any[]>([]);
+
+  useEffect(() => {
+    // Fetch data
+    const endpoint =
+      'https://gist.githubusercontent.com/huma-OS/43c02bc3d7642f4fb65f87e2c45264db/raw/eb6eb6f5e1dc9c8d4ebdd9fd5f27c2df94d799e4/africa.json';
+
+    fetch(endpoint)
+      .then(blob => blob.json())
+      .then(data => setCities(data));
+  }, []);
+
+  useEffect(() => {
+    if (query) {
+      const regex = new RegExp(query, 'gi');
+      const matchArray = cities.filter(place =>
+        place.city.match(regex) || place.country.match(regex)
+      );
+      setFilteredCities(matchArray);
+    } else {
+      setFilteredCities([]);
+    }
+  }, [query, cities]);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
   return ( 
-    <div>
-      <h1>AFRICA</h1>
-      <p>This is a more elaborate web development app for page 1.</p>
-      {/* Add more components and functionality as needed */}
-    </div>
+    <>
+    <div className="tech-stack-icons web-dev-icons">
+            {/* Tech Stack Icons */}
+            <img
+              className="tech-icon"
+              src="/img/icons/react.svg"
+              alt="React"
+            />
+            <img
+              className="tech-icon"
+              src="/img/icons/typescript.svg"
+              alt="TypeScript"
+            />
+            <img
+              className="tech-icon"
+              src="/img/icons/npm.svg"
+              alt="npm"
+            />
+             <img
+              className="tech-icon"
+              src="/img/icons/git.svg"
+              alt="git"
+            />
+             <img
+              className="tech-icon"
+              src="/img/icons/github.svg"
+              alt="github"
+            />
+            {/* Add more icons here */}
+          </div>
+          <form className="search-form">
+      <input
+        type="text"
+        className="search"
+        placeholder="African City or Country"
+        value={query}
+        onChange={handleInputChange}
+      />
+      <ul className="suggestions">
+        {filteredCities.map((place, index) => {
+          const regex = new RegExp(query, 'gi');
+          const cityName = place.city.replace(regex, `<span class="hl">${query}</span>`);
+          const countryName = place.country.replace(regex, `<span class="hl">${query}</span>`);
+          return (
+            <li key={index}>
+              <div className="name" dangerouslySetInnerHTML={{ __html: `${cityName}, ${countryName}` }}></div>
+              <div className="population">Population: {place.population}</div>
+              <div className="population">Language: {place.language}</div>
+              <div className="population">GDP: {place.gdp}</div>
+              <div className="population">Size: {place.size}</div>
+            </li>
+          );
+        })}
+      </ul>
+    </form>
+    </>
+    
    );
 }
  
